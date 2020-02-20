5e36e528d6974739a34189302b728252530313af
oyeyipo45 <oyeyipo45@gmail.com>
2020-02-20 17:03:05 +0100

    remains reload

--- script.js
+++ script.js
@@ -1,52 +1,128 @@
-let input = document.querySelector("input");
-//getting the input element by decleariation
+let input = document.getElementById("inputField");
 let imagesHolder = document.getElementById("imagesHolder");
+let search = document.getElementById("Search"); 
 
 
-
-
-const findGiffy = e =>{
-    //the function that runs the fetch and conversion to json
-    console.log("looking", e.target.value);
-    //logging the value of the target of the "e"(event) from the keypress
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=5DpBIKt6t2wx0Kr8uJLuUMB3xYuVPbqU&q=smh&limit=25&offset=0&rating=R&lang=en"). //fetching the Api from the server
-    then(res => res.json()). //converting the result to jsn format
-    then(result => { // functioning the result to do sometjing and in this case it it just to console.log the the value if the search  
+const findGiffy = () => {
+    input = input.value;
+    console.log( input);
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=5DpBIKt6t2wx0Kr8uJLuUMB3xYuVPbqU&q=" + input + "&limit=20&offset=0&rating=R&lang=en"). //fetching the Api from the server WITH the "e.target.value" serving as the value of what ever you type
+    then(res => res.json()).
+    then(result => {  
         console.log(result, "result"); 
-        
-        result.data.forEach(data => {
-            //after getting the result this function goes through with "forEach", to effect the change on the data that requested from the array 
-            let image = data.images.downsized_large;
-            //declaring the element image for better typography instead of writing the whole thing everytime 
+        result.data.forEach(function(dami) { 
             let img = document.createElement("img");
-            //creating the "img" that will house the rsults when I want to append it to the node
-            img.src = "https://media3.giphy.com/media/9DJtFRgk0tOla/giphy.gif?cid=202df742ae0bde770d38181c3091e116ea7ca7d66107d866&rid=giphy.gif";
-            //declaring the source of the image
-            img.src = image.url;
-            //assigning the url of the image to the image source
+            img.width = 350;
+            img.height = 300;
+            img.src = dami.images.downsized.url;
             imagesHolder.appendChild(img);
-            //appending the image element that was created to the "imagesHolder" node
         });
         
     })
-}; //on every search this function block runs there by making this call
+}; 
 
 
-//declaring the target to the function 
-input.onkeypress = findGiffy;
+// input.onkeypress = findGiffy;
+
+let resetButton  = () => {
+    let input = document.getElementById("inputField");
+    input.value = " ";
+}
+
+let reset = document.getElementById("button");
+reset.addEventListener("click", resetButton);
+search.addEventListener("click", findGiffy);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input = document.getElementById("inputField");
+// //getting the input element by decleariation
+// let imagesHolder = document.getElementById("imagesHolder");
+// //declearing the parent node
+// let search = document.getElementById("Search"); 
+
+// const searchFunction = () =>{
+//     console.log("www");
+//     console.log(input.value);
+// };
+
+
+
+
+
+// const findGiffy = e => {
+//     //the function that runs the fetch and conversion to json
+    
+//     console.log( e.target.value);
+//     //logging the value of the target of the "e"(event) from the keypress
+//     fetch("https://api.giphy.com/v1/gifs/search?api_key=5DpBIKt6t2wx0Kr8uJLuUMB3xYuVPbqU&q=" + e.target.value + "&limit=&offset=0&rating=R&lang=en"). //fetching the Api from the server WITH the "e.target.value" serving as the value of what ever you type
+//     then(res => res.json()). //converting the result to jsn format
+//     then(result => { // functioning the result to do sometjing and in this case it it just to console.log the the value if the search  
+//         console.log(result, "result"); 
+        
+//         result.data.forEach(function(dami) { //declaring the parameter as dami which is an array of objects
+//             //after getting the result this function goes through with "forEach", to effect the change on the data that requested from the array 
+//             let img = document.createElement("img");
+//             //creating the "img" that will house the rsults when I want to append it to the node
+//             img.width = 350;
+//             img.height = 300;
+//             img.src = dami.images.downsized.url;
+//             //declaring the source of the image and getting the source from inside the array of object which is dami
+//             imagesHolder.appendChild(img);
+//             //appending the image element that was created to the "imagesHolder" node
+//         });
+        
+//     })
+// }; //on every search this function block runs there by making this call
+
+
+// //declaring the input keypress to the function 
+// input.onkeypress = findGiffy;
+
+
+// let resetButton  = () => {
+//     input.value = " ";
+// }
+// //function to reset the input value
+
+// let reset = document.getElementById("button");
+// reset.addEventListener("click", resetButton);
+// //adding event listener to the reset button
 
 
  
 
+// search.addEventListener("click", searchFunction );
 
 
-
-
-// let but = document.getElementById("button");
-// let press = () => {
-//     console.log("working");
-// };
-// but.addEventListener("click", press);
